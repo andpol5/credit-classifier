c691d00a9cc4e7480ff7976b99bd8ccea11a98f6
andpol5 <and.pol5@gmail.com>
2016-01-22 14:12:50 +0100

    Add working neural net with 2 hidden layers

--- trainNeuralNet.py
+++ trainNeuralNet.py
@@ -18,10 +18,20 @@ def random_batch(dataset, batch_size):
     y = sample[:,last_col_index:last_col_index+2]
     return (x, y)
 
+# Tensorflow convinience functions
+def weight_variable(shape):
+  initial = tf.truncated_normal(shape, mean=0.001, stddev=0.3)
+  return tf.Variable(initial)
+
+def bias_variable(shape):
+  initial = tf.constant(0.1, shape=shape)
+  return tf.Variable(initial)
+
 # Constants
 learning_rate = 1e-4
 num_classes = 2
 batch_size = 100
+dropout = 0.5
 
 false_neg_cost = 1.0
 false_pos_cost = 5.0
@@ -36,16 +46,31 @@ x_width = cols-2
 # Placeholder values
 x = tf.placeholder(tf.float32, [None, x_width])
 
-# Try linear regression as first attempt
-W = tf.Variable(tf.zeros([x_width, num_classes]))
-b = tf.Variable(tf.zeros([num_classes]))
+# Neural network with 2 hidden layers
+
+# Fully connected layer 1:
+w_fc1 = weight_variable([x_width, 512])                   # weights
+b_fc1 = bias_variable([512])                          # biases
+h_fc1 = tf.nn.relu(tf.matmul(x, w_fc1) + b_fc1)       # activation
+keep_prob = tf.placeholder("float")
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)          # dropout
+
+# Fully connected layer 2:
+w_fc2 = weight_variable([512, 512])
+b_fc2 = bias_variable([512])
+h_fc2 = tf.nn.relu(tf.matmul(h_fc1_drop, w_fc2) + b_fc2)
+h_fc2_drop = tf.nn.dropout(h_fc2, keep_prob)
+
+# Readout layer
+w_fc3 = weight_variable([512, num_classes])
+b_fc3 = bias_variable([num_classes])
 
 # The softmax function will make probabilties of Good vs Bad score at the output
-y_ = tf.nn.softmax(tf.matmul(x, W) + b)
-
+y_ = tf.nn.softmax(tf.matmul(h_fc2_drop, w_fc3) + b_fc3)
 y = tf.placeholder(tf.float32, [None, num_classes])
 
-# Training:
+# Training
+# Different loss functions:
 # cross_entropy =  -tf.reduce_sum(y_ * tf.log(tf.clip_by_value(y, 1e-15, 1.0)))
 
 # Mofified mean square error function which uses a cost of 5 for false positives
@@ -63,13 +88,13 @@ sess = tf.InteractiveSession()
 sess.run(init)
 for i in range(5000):
   batch_xs, batch_ys = random_batch(dataset, batch_size)
-  sess.run(train_step, feed_dict={x: batch_xs, y: batch_ys})
+  sess.run(train_step, feed_dict={x: batch_xs, y: batch_ys, keep_prob: dropout})
 
 correct_prediction = tf.equal(tf.argmax(y_,1), tf.argmax(y,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 
 y_p = tf.argmax(y_,1)
-accuracyD, yP = sess.run([accuracy, y_p], feed_dict={x: dataset[:,0:59], y: dataset[:,59:61]})
+accuracyD, yP = sess.run([accuracy, y_p], feed_dict={x: dataset[:,0:59], y: dataset[:,59:61], keep_prob: 1.0})
 print("Accuracy: %f" % accuracyD)
 
 # Get the confusion matrix
